197cef219d819d1299f2b7283e679c75809829df
Bruno Gusson <85442937+BrunoGusson@users.noreply.github.com>
2024-05-05 19:49:35 -0300

    Add files via upload

---
+++ aula08.js
@@ -0,0 +1,65 @@
+//Exercitando Arrays e funções
+//Exeercicio 1: Somar todos os números de um array
+
+// let  numeros = [20, 10 , 30]
+
+// function somaNumeros(vetor){
+//     let soma = 0
+//     for(let i = 0; i < vetor.length; i++){
+//         soma += vetor[i]
+//     }
+//     return soma
+// }
+// console.log("A soma dos números é:", somaNumeros(numeros))
+
+
+//Encontrar o maior número de um Array
+
+// let num = [65, 34, 70, 100]
+//     let maiornum = 0
+// function maiorNum(vetor){
+//     for(let i = 0; i >00 )
+// }l
+
+
+
+  //Exercício 2: Encontrar o maior número em um array
+// function maiorNumero(array) {
+//     let maior = array[0];
+//     for (let i = 1; i < array.length; i++) {
+//         if (array[i] > maior) {
+//             maior = array[i];
+//         }
+//     }
+//     return maior;
+// }
+
+// console.log("Teste Maior Número:", maiorNumero([1, 22, 3, 4]));    // Deve retornar 22
+
+
+
+  //Exercício 3: Reverter um array
+// function inverteArray(array) {
+//     let resultado = [];
+//     for (let i = array.length - 1; i >= 0; i--) {
+//         resultado.push(array[i]);
+//     }
+//     return resultado;
+// }
+
+// console.log("Teste Inverte Array:", inverteArray([1, 2, 3, 4]));  // Deve retornar [4, 3, 2, 1]
+
+
+
+// Exercicio :  Somar todos os elementos de uma matriz
+
+// // function somaMatriz(matriz){
+// //     let soma = 0 
+// //     for( let i = 0; i <matriz.length; i++){
+// //       for( let j = 0; j  < matriz[i].length; j++){
+// //         soma += matriz[i][j]
+// //       }
+// //      }
+// //     return soma 
+// // }
+// // console.log("A soma dos elementos contidos na nossa matriz é:" , somaMatriz([[1, 2] [2,4]]))
